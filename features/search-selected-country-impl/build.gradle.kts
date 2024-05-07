80d1ae97adb1024361b1c084e7589e8694faf0b3
Mikhail Pisegov <pisegov.mikhail@gmail.com>
2024-05-07 10:11:43 +0300

    add selected country search screen modules and layout

---
+++ features/search-selected-country-impl/build.gradle.kts
@@ -0,0 +1,51 @@
+plugins {
+    alias(libs.plugins.androidLibrary)
+    alias(libs.plugins.jetbrainsKotlinAndroid)
+    alias(libs.plugins.kapt)
+}
+
+android {
+    namespace = "com.myaxa.search_selected_country_impl"
+    compileSdk = 34
+
+    defaultConfig {
+        minSdk = 27
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.navigation.fragment.ktx)
+    implementation(libs.adapterdelegates)
+
+    implementation(libs.dagger)
+    kapt(libs.dagger.compiler)
+
+    implementation(project(":features:search-selected-country-api"))
+    implementation(project(":domain"))
+    implementation(project(":common"))
+
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+}
